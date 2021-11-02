3b4700a597ee6c91aee79b4df0f491a9c423f4f8
vicky kumar <vickyjnv7@gmail.com>
2021-11-03 01:06:49 +0530

    initial started project

---
+++ lib/pages/user_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class UserMain extends StatefulWidget {
+  const UserMain({Key? key}) : super(key: key);
+
+  @override
+  _UserMainState createState() => _UserMainState();
+}
+
+class _UserMainState extends State<UserMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
